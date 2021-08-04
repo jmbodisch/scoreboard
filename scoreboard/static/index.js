dd203093b2d85cd249316826c84b37041b067982
jmbodisch <boingloing@gmail.com>
2021-08-04 00:59:18 -0400

    javascript to increment or decrement ints

--- scoreboard/static/index.js
+++ scoreboard/static/index.js
@@ -1,9 +1,33 @@
 function update(fileName) {
-    var newValue = document.getElementById(fileName).value;
-    var request = new XMLHttpRequest();
+    let newValue = document.getElementById(fileName).value;
+    let request = new XMLHttpRequest();
     request.open("GET", '/update/' + fileName + '/' + newValue);
     request.onload = function() {
         document.getElementById(fileName).value = request.responseText;
+    };
+    request.send('');
+}
+
+function increment(fileName) {
+    let request = new XMLHttpRequest();
+    request.open("GET", '/' + fileName);
+    request.onload = function() {
+        let num = parseInt(request.responseText);
+        num++;
+        document.getElementById(fileName).value = num;
+        update(fileName);
+    };
+    request.send('');
+}
+
+function decrement(fileName) {
+    let request = new XMLHttpRequest();
+    request.open("GET", '/' + fileName);
+    request.onload = function() {
+        let num = parseInt(request.responseText);
+        num--;
+        document.getElementById(fileName).value = num;
+        update(fileName)
     };
     request.send('');
 }
