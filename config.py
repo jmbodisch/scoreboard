931953ce2886f5939739059bd97d3ead572e7413
jmbodisch <boingloing@gmail.com>
2021-08-05 23:07:53 -0400

    Not really using this right now

--- config.py
+++ config.py
@@ -14,7 +14,3 @@ class TestConfig(Config):
 class DevelopmentConfig(Config):
     TESTING = True
     DEBUG = True
-    SCORE_A=r"C:\Users\boing\Documents\Splatoon\OBS\Thembo IHOP\score1.txt"
-    SCORE_B=r"C:\Users\boing\Documents\Splatoon\OBS\Thembo IHOP\score2.txt"
-    TEAM_A=r"C:\Users\boing\Documents\Splatoon\OBS\Thembo IHOP\team1.txt"
-    TEAM_B=r"C:\Users\boing\Documents\Splatoon\OBS\Thembo IHOP\team2.txt"
