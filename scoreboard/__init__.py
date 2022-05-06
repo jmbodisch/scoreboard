7a2d1dee9205d54eb72fed8595d4853894d2edd3
jmbodisch <boingloing@gmail.com>
2022-05-06 14:37:06 -0400

    I am not using the db lol

--- scoreboard/__init__.py
+++ scoreboard/__init__.py
@@ -28,11 +28,7 @@ def create_app(test_config=None):
 
     app = Flask(__name__)
     CORS(app)
-
-    app.config.from_mapping(
-        DATABASE=os.path.join(app.instance_path, 'scoreboard.sqlite')
-    )
-
+    
     app.config.from_object('config.DevelopmentConfig')
 
     # ensure the instance folder exists
